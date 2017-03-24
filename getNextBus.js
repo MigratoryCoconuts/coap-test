f6c54c017966f20b77d163f1eb763398427926c5
Flo Everett <jtatgfu@gmail.com>
2017-03-24 17:51:07 +1300

    bug fixes, coap functionality

--- getNextBus.js
+++ getNextBus.js
@@ -1,14 +1,13 @@
 var mongodb = require('mongodb')
 	, MongoClient = mongodb.MongoClient
 	, co = require('co')
-  ,	test = require('assert');
+  ,	test = require('assert')
 	
 function timeStr2Secs(str) {
 	if (typeof str == "number" && str < 24*3600) return str;
-	var flds = str.split(/\W/);
-		
-	return ~~flds[0] * 3600 + ~~flds[1] * 60 + (~~flds[2] || 0);
 	
+	var flds = str.split(/\W/);	
+	return ~~flds[0] * 3600 + ~~flds[1] * 60 + (~~flds[2] || 0);
 }
 
 function secsSinceMidnight(d) {
@@ -16,7 +15,6 @@ function secsSinceMidnight(d) {
 	return ~~((d - e.setHours(0,0,0,0))/1000);
 }
 
-//console.log(mongodb.Collection.prototype);
 
 function aggregateToCollection(col, pipeline, outCol, options) {
 	test(pipeline instanceof Array);
@@ -36,65 +34,110 @@ function aggregateToCollection(col, pipeline, outCol, options) {
 	});
 }
 
+function formatDateStr(d) {
+	var str = d+"";
+	return str.slice(0,4)+'-'+str.slice(4,6)+'-'+str.slice(6,8)
+}
+
+function isInDateRange(d, start, end) {
+	start = Date.parse(formatDateStr(start));
+	if (isNaN(start)) start = 0;
+
+	end = Date.parse(formatDateStr(end)+" 23:59:59.999");
+	if (isNaN(end)) end = Infinity;
+	
+	return (start <= d.valueOf()) && (d.valueOf() <= end);
+}
+
 function posMod(a,b) {
-return ((a%b)+b)%b;
+	return ((a%b)+b)%b;
 }
 
 var requestStop = "1719"
 	, requestTime = new Date("2017/03/27 13:33:37");
 	
-console.log(requestTime.toLocaleString());
+
+var isValidStop = co.wrap(function*(stop_id){
+	var db = yield MongoClient.connect('mongodb://localhost:27017/data');
+	return (yield db.collection("stops").find({stop_id:stop_id}).count()) > 0;
+});
 	
 var getNextBus = co.wrap(function*(stop_id,time) {
 	var db = yield MongoClient.connect('mongodb://localhost:27017/data');
 	
-	console.log("connected");
 	
 	var now = secsSinceMidnight(time)
-		, doyOfWeek = 
-		, UID = "test" // Date.now().toString()									//HEY make UID better
+		, dayOfWeek = (["sunday","monday","tuesday","wednesday","thursday","friday","saturday"])[time.getDay()]
+		, UID = "temp"
 		, i = 0;
 		
-	//yield db.collection(UID).drop();	
-		
 	yield aggregateToCollection(db.collection("stoptimes"), [
-		{$match:{stop_id:stop_id}},	
+		{$match:{stop_id:stop_id, arrival_time:{$ne:""}}},	
 		{$lookup:{
 			from: "trips",
 			localField: "trip_id",
 			foreignField: "trip_id",
 			as: "trip_doc"
 		}},
-		{$project:{arrival_time:1, trip_id:1, stop_sequence:1, route_id:"$trip_doc.route_id", service_id:"$trip_doc.service_id"}},
+		{$project:{arrival_time:1, route_id:"$trip_doc.route_id", service_id:"$trip_doc.service_id"}},
 		{$unwind:"$route_id"},
 		{$unwind:"$service_id"},
 		{$group:{
-			
-		}}
+			_id: "$service_id",
+			arrival_time: {$addToSet:"$arrival_time"},
+			route_id: {$addToSet:"$route_id"}
+		}},
+		{$unwind:"$route_id"},
+		{$unwind:"$arrival_time"},
+		{$lookup:{
+			from: "calendars",
+			localField:"_id",
+			foreignField:"service_id",
+			as:"service_doc"
+		}},
+		{$project:{start_date:"$service_doc.start_date", end_date:"$service_doc.end_date", arrival_time:1, route_id:1, today:`$service_doc.${dayOfWeek}`,_id:{at:"$arrival_time",sid:"$_id",rid:"$route_id"}}},
+		{$unwind:"$start_date"},
+		{$unwind:"$end_date"},
+		{$unwind:"$today"},
+		{$match:{"today":1}}
 	], UID);
 	
-	console.log("got stop docs");
 	
-	var c = db.collection(UID).find();
+	var c = db.collection(UID).find()
+		, ops = [];
 	
-	console.log(yield c.count());
 	
 	while (yield c.hasNext()){
 		var doc = yield c.next();
-		if (doc) yield db.collection(UID).updateOne({_id:doc._id},
-			{$set:{time_to_wait: posMod(timeStr2Secs(doc.arrival_time)-now,3600*24)}}
-		);
+		ops.push({
+			updateOne: {
+				filter: {
+					_id:doc._id
+				},
+				update: {
+					$set: {
+						time_to_wait: posMod(timeStr2Secs(doc.arrival_time)-now,3600*24),		//this will return this morning's buses, needs reworking
+						isRunning: isInDateRange(time, doc.start_date, doc.end_date)
+					}
+				}
+			}
+		});
 	}
-	console.log("added time_to_wait fields");
-	var next5 = db.collection(UID).aggregate([
-		{$sort:{time_to_wait:1}},
-		{$limit:5},
-		//{$out:UID}
-	]);
+	yield db.collection(UID).bulkWrite(ops);
 	
-	console.dir(yield next5.toArray());
-	//	db.collection(UID).drop();
-	db.close();
+	var next5 = db.collection(UID).aggregate([
+		{$match:{isRunning:true, time_to_wait:{$lt:3600}}},
+		{$sort:{time_to_wait:1}},
+		{$project:{_id:0, route_id:1, arrival_time:1, time_to_wait:1}},
+		{$limit:5},
+	]);
+	var out = yield next5.toArray()
+	yield db.close();
+	return out;
 });
 
-getNextBus(requestStop, requestTime).catch(err=>{console.error(err.stack)});
+//getNextBus(requestStop, new Date())
+//	.then(list => {console.dir(list)})
+//	.catch(err=>{console.error(err.stack)});
+
+module.exports = {getNextBus: getNextBus, isValidStop: isValidStop};
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const coap    = require('coap') // or coap
     , server  = coap.createServer()
+		, readDb 	= require('./getNextBus.js')
+		, co 			= require("co");
+		
+var handleRequest = co.wrap(function*(req, res) {
+	 //if (req.headers['Observe'] !== 0)
+		
+	try {
+		var stop = req.url.split('/')[1]
+			, now = new Date();
+		console.log(`Incoming request for stop ${stop}`);
+		var isValid = yield readDb.isValidStop(stop)
+		console.log(`${stop} is ${isValid?"":"not"} a valid stop`);
+		
+		if (isValid) {
+			var nextBuses = yield readDb.getNextBus(stop, now);
+			res.setOption('Content-Format', 'application/json');
+			res.write(JSON.stringify(nextBuses))
+			res.end(() => {console.log(`Responded with data after ${Date.now() - now.valueOf()} ms`)});
+		} else {
+			res.code = 404;
+			res.end(() => console.log(`Responded with 404`));
+		}
+	} catch(err) {
+		res.code = 500;
+		console.trace(err);
+		res.end(() => console.log(`There was an error! responded with 500`));
+	}
+});
 
-server.on('request', function(req, res) {
-  /*if (req.headers['Observe'] !== 0)
-    return res.end(new Date().toISOString() + '\n')
-
-  var interval = setInterval(function() {
-    res.write(new Date().toISOString() + '\n')
-  }, 1000)
-
-  res.on('finish', function(err) {
-    clearInterval(interval)
-  })*/
-	
-	res.end(/*'Hello ' + req.url.split('/')[1] + '\nMessage payload:\n'+req.payload+'\n'*/);
-	console.dir(res);
-})
+server.on('request', handleRequest);
 
 server.listen(function() {
   console.log('server started')
